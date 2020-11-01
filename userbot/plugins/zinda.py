92d050304a62e97a6a7633cdc9dd4e431e27796c
Marshmello-op <71318599+shubhanshdj@users.noreply.github.com>
2020-11-01 08:33:31 +0530

    Update zinda.py

--- userbot/plugins/zinda.py
+++ userbot/plugins/zinda.py
@@ -4,18 +4,20 @@ from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/91a00acf67d80235e11f0.jpg"
+PM_IMG = "https://telegra.ph/file/c735db5b11ded78784257.jpg"
 pm_caption = "**𐌑𐌀𐍂𐍃𐋏𐌑𐌄𐌋𐌋𐍈 is ONLINE\n"
 
-pm_caption += "i am ALIVE MY MASTER.....
+pm_caption += "ι αм αℓινє мγ ρєяο мαѕτєя ωιτн иєϲєѕѕαяγ ∂γиοѕ 😘\n"
 
 pm_caption += "sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ           : [ᴊᴏɪɴ](https://t.me/marshmellobot_official)\n"
 
 pm_caption += "sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ        : [ᴊᴏɪɴ](https://t.me/marshmello_userbot)\n"
 
-pm_caption += "ᴘʏᴛʜᴏɴ       : 3.5.8 ᴘʏᴛʜᴏɴ
+pm_caption += "ᴘʏᴛʜᴏɴ                  : 3.5.8 ᴘʏᴛʜᴏɴ
 
-pm_caption += " [](https://t.me/pycodingwithayush)"
+pm_caption += f"**My Master**        : {DEFAULTUSER}\n"
+
+pm_caption += " [██████╗░░░░░░██╗\n ██╔══██╗░░░░░██║\n ██║░░██║░░░░░██║\n ██║░░██║██╗░░██║\n ██████╔╝╚█████╔╝\n ╚═════╝░░╚════╝░\n(https://t.me/Marshmello_userbot)"
 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
