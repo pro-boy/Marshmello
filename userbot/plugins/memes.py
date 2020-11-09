028a51a925f358cef7d2b45f34703ae57a9ad445
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-09 22:43:52 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -850,7 +850,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/DARK-COBRA/DARKCOBRA) to open this cool userbot repo 𝔻𝔸ℝ𝕂 ℂ𝕆𝔹ℝ𝔸.")
+        await e.edit("Click [here](https://github.com/shubhanshdj/Marshmello) to open this cool userbot repo😎.")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
