b1921a18e5439f02feb02f8127c406a5898fe267
Marshmello-op <71318599+shubhanshdj@users.noreply.github.com>
2020-11-19 17:00:20 +0530

    Bhai bhai bhI bhai bhai bhai bhai

---
+++ userbot/plugins/pinng.py
@@ -0,0 +1,35 @@
+#Credit To @Beast_boy_shubu . Keep credit if you are going to edit it.
+
+
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="ping ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("✨🎶Ping")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶✨🎶")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶✨🎶Ping")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶✨🎶Ping✨🎶Ping")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶✨🎶Ping✨🎶Ping✨🎶✨🎶")
+        await asyncio.sleep(0.4)
+        await event.edit("✨🎶Ping✨🎶Pong✨🎶✨🎶✨🎶Ping\n✨🎶Pong✨🎶✨🎶Ping✨🎶Ping✨🎶✨🎶Pong✨🎶...")
+        
