3af4f2bfcafbac23aaa45717220b2abcc611efeb
Marshmello-op <71318599+shubhanshdj@users.noreply.github.com>
2020-11-20 21:35:35 +0530

    Lawda logo ?

---
+++ userbot/plugins/aporn.py
@@ -0,0 +1,158 @@
+# 🖕🖕🖕🖕🖕🖕🖕🖕🖕
+# 🤖⚙️🤖⚙️🤖⚙️🤖⚙️🤖
+# 💦💦💦💦💦💦💦💦💦
+# HeHeHe  adult plugin is here lol
+# 👄👅💦👄👅💦👄👅💦👄👅💦👄👅💦👄👅💦 
+# Created by @danish_00
+# 😅👉🏻👌✨💦👉🏻👌✨💦👉🏻👌✨💦
+# Ab maza aayega na bidu 😂😂
+# Mi Iz pIrO 😂😂😂 
+
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern="aboobs ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Epornerbot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=105460780))
+              await event.client.send_message(chat, "/boobs")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Epornerbot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("`Sorry sir, That @Epornerbot is dead now`")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+
+
+@borg.on(admin_cmd(pattern="abutts ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Epornerbot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=105460780))
+              await event.client.send_message(chat, "/butt")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Epornerbot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("`Sorry sir, That @Epornerbot is dead now`")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+
+@borg.on(admin_cmd(pattern="agifs ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Epornerbot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=105460780))
+              await event.client.send_message(chat, "/gif")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Epornerbot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("`Sorry sir, That @Epornerbot is dead now`")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+@borg.on(admin_cmd(pattern="avideos ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Gufabot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1304516644))
+              await event.client.send_message(chat, "🤩")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Gufabot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("Sorry sir, That @Gufabot is dead now")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+@borg.on(admin_cmd(pattern="afun ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Gufabot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1304516644))
+              await event.client.send_message(chat, "🍆")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Gufabot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("Sorry sir, That @Gufabot is dead now")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+
+# This Cmd is Not working in that Epornerbot bot so untill then lets leave this for future use hehe😂
+
+"""
+
+@borg.on(admin_cmd(pattern="agirl ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    
+    reply_message = await event.get_reply_message()
+    chat = "@Epornerbot"
+    await event.edit("Checking...")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=105460780))
+              await event.client.send_message(chat, "/random")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Boss! Please Unblock @Epornerbot ")
+              return
+          if response.text.startswith(" "):
+             await event.edit("`Sorry sir, That @Epornerbot is dead now`")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
+
+"""
+#😚😚😚😚
