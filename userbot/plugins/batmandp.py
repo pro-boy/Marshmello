20a51f930f64f4dcca8f0e19683e774939b8bcb2
Marshmello-op <71318599+pro-boy@users.noreply.github.com>
2020-12-08 15:10:17 +0530

    me noobv

---
+++ userbot/plugins/batmandp.py
@@ -0,0 +1,39 @@
+import requests , re , random 
+import urllib , os 
+from telethon.tl import functions
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from userbot.utils import admin_cmd
+import asyncio
+from time import sleep
+
+COLLECTION_STRING = [
+  "awesome-batman-wallpapers",
+  "batman-arkham-knight-4k-wallpaper",
+  "batman-hd-wallpapers-1080p",
+  "the-joker-hd-wallpaper",
+  "dark-knight-joker-wallpaper"
+]
+
+async def animepp():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+    pack = COLLECTION_STRING[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+@borg.on(admin_cmd(pattern="batmandp ?(.*)"))
+async def main(event):
+    await event.edit("**Starting batman Profile Pic.**\n Done done done🤙") #Owner @NihiNivi
+    while True:
+        await animepp()
+        file = await event.client.upload_file("donottouch.jpg")
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+        await asyncio.sleep(600) #Edit this to your required needs
