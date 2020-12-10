4060e0f0ecb11742df92079258f7454786d9363d
Marshmello-op <71318599+pro-boy@users.noreply.github.com>
2020-12-10 11:09:34 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -211,7 +211,7 @@ async def fetch_info(replied_user, event):
     try:
         dc_id, location = get_input_location(replied_user.profile_photo)
     except:
-        dc_id = "Couldn't fetch DC ID!"
+        dc_id = "Couldn't fetch ID!"
     common_chat = replied_user.common_chats_count
     username = replied_user.user.username
     user_bio = replied_user.about
@@ -231,18 +231,18 @@ async def fetch_info(replied_user, event):
     )
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
-    caption = "<b>USER INFO from MARSHMELLO's database :</b>\n\n"
-    caption += f"👤First Name: {first_name} {last_name}\n"
-    caption += f"🤵Username: {username}\n"
-    caption += f"🔖ID: <code>{user_id}</code>\n"
-    caption += f"🌏Data Centre ID: {dc_id}\n"
-    caption += f"🖼Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"🤖Is Bot: {is_bot}\n"
-    caption += f"🔏Is Restricted: {restricted}\n"
-    caption += f"🌐Is Verified by Telegram: {verified}\n\n"
-    caption += f"✍️Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"👥Common Chats with this user: {common_chat}\n"
-    caption += f"🔗Permanent Link To Profile: "
+    caption = "<b>╒═══ 「 Userinfo By Marshmello: 」</b>\n\n"
+    caption += f"First Name: {first_name} {last_name}\n"
+    caption += f"Username: {username}\n"
+    caption += f"ID: <code>{user_id}</code>\n"
+    caption += f"Data Centre ID: {dc_id}\n"
+    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"Is Bot: {is_bot}\n"
+    caption += f"Is Restricted: {restricted}\n"
+    caption += f"Is Verified by Telegram: {verified}\n\n"
+    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"╘══「 Groups Counted: {common_chat} 」\n"
+    caption += f"🔗Permalink: "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
     return photo, caption
 
